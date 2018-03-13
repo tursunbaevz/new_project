d8793a5a219316aab6592f60e4ccb1f5b3189a9d
Kozhonazar <rysbaev.k@gmail.com>
2018-03-13 18:26:24 +0600

    Hello
    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello World!
+                    Hello Zuma!
                 </div>
 
                 <div class="links">
