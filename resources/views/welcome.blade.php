e5f00b569ae0890473c7e1c058fc181b4d4d0ce3
tursunbaevz <zuma9405@gmail.com>
2018-03-14 19:36:34 +0600

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello World!
+                    Work Hard EPTA! 
                 </div>
 
                 <div class="links">
