fa167745aaabbfd3ffb4df2c193053bcd0455afc
Kozhonazar <rysbaev.k@gmail.com>
2018-03-14 23:14:37 +0600

    push-test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Work Hard EPTA! 
+                    Sam takoi! EPTA! 
                 </div>
 
                 <div class="links">
