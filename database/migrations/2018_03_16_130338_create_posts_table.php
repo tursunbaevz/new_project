2f147a95cc64b263b04e36b1f9740604c5185dc1
tursunbaevz <zuma9405@gmail.com>
2018-03-17 17:25:11 +0600

    add new header table

--- database/migrations/2018_03_16_130338_create_posts_table.php
+++ database/migrations/2018_03_16_130338_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
+            $table->text('header');
             $table->text('body');
             $table->timestamps();
         });
